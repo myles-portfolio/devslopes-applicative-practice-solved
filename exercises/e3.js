d17d93ca1b8b3e382e0e16551e22d3092e4f889e
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-04-10 12:04:30 -0500

    completed e3 and e4

--- exercises/e3.js
+++ exercises/e3.js
@@ -5,7 +5,10 @@ import { data } from "../data/data";
 // Return example: [1, 2, ... , N]
 
 export function getAllAverageTemperatures(data) {
-  // Your code goes here...
+  const planets = data.planets;
+  return planets.map(function(planet) {
+  return planet.avgTemp;
+  });
 }
 
 
--- exercises/e4.js
+++ exercises/e4.js
@@ -5,7 +5,13 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsWithLowGravity(data) {
-  // Your code goes here...
+  const planets = data.planets;
+  return planets.filter(function(planet) {
+    return planet.gravity < 10;
+  })
+    .map(function(planet) {
+      return planet.name;
+    });
 }
 
 
